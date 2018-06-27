1d471f206e9cfdfd459e193493c13eae345b2aeb
colin_mark <colin.alcorn@gmail.com>
2018-06-27 01:12:22 -0500

    User Story 3 - User Profile: completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  # resources :users, only: [:show]
+
   # get 'welcome/index'
 
-  devise_for :users
+  # devise_for :users
+  devise_for :users, :path_prefix => 'd'
+  resources :users, :only => [:show]
 
-  root "welcome#index"
+  root "users#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
